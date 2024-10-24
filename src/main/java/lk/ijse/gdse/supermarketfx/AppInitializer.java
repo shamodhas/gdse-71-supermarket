8efe44a71b6df4e0f1ca75c868443908fb9b248b
shamodhasahan <shamodhasrathnamalala@gmail.com>
2024-10-24 11:27:20 +0530

    initial commit

--- src/main/java/lk/ijse/gdse/supermarketfx/AppInitializer.java
+++ src/main/java/lk/ijse/gdse/supermarketfx/AppInitializer.java
@@ -3,6 +3,7 @@ package lk.ijse.gdse.supermarketfx;
 import javafx.application.Application;
 import javafx.concurrent.Task;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
@@ -13,15 +14,39 @@ import java.io.IOException;
 public class AppInitializer extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(AppInitializer.class.getResource("/view/MainLayout.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Supermarket FX");
-
-        Image image = new Image(getClass().getResourceAsStream("/images/app_icon.png"));
-        stage.getIcons().add(image);
-
-        stage.setScene(scene);
+        Parent load = FXMLLoader.load(getClass().getResource("/view/LoadingScreenView.fxml"));
+        stage.setScene(new Scene(load));
         stage.show();
+
+       Task<Scene> loadingTask = new Task<Scene>() {
+           @Override
+           protected Scene call() throws Exception {
+               FXMLLoader fxmlLoader = new FXMLLoader(AppInitializer.class.getResource("/view/MainLayout.fxml"));
+               return new Scene(fxmlLoader.load());
+           }
+       };
+
+       loadingTask.setOnSucceeded(event -> {
+           Scene value = loadingTask.getValue();
+
+           stage.setTitle("Supermarket FX");
+           Image image = new Image(getClass().getResourceAsStream("/images/app_icon.png"));
+           stage.getIcons().add(image);
+
+           stage.setScene(value);
+       });
+
+       new Thread(loadingTask).start();
+
+//        FXMLLoader fxmlLoader = new FXMLLoader(AppInitializer.class.getResource("/view/MainLayout.fxml"));
+//        Scene scene = new Scene(fxmlLoader.load());
+//        stage.setTitle("Supermarket FX");
+//
+//        Image image = new Image(getClass().getResourceAsStream("/images/app_icon.png"));
+//        stage.getIcons().add(image);
+//
+//        stage.setScene(scene);
+//        stage.show();
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/lk/ijse/gdse/supermarketfx/controller/LoadingScreenController.java
@@ -0,0 +1,15 @@
+package lk.ijse.gdse.supermarketfx.controller;
+
+/**
+ * --------------------------------------------
+ * Author: R.I.B. Shamodha Sahan Rathnamalala
+ * GitHub: https://github.com/shamodhas
+ * Website: https://shamodha.live
+ * --------------------------------------------
+ * Created: 10/24/2024 10:59 AM
+ * Project: supermarketfx-71
+ * --------------------------------------------
+ **/
+
+public class LoadingScreenController {
+}
